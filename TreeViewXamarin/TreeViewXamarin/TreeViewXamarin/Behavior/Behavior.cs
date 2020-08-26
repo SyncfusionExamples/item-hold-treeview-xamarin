525e13b2ab8e8143b2dc87b7797cfb539a7216e7
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-08-26 17:51:30 +0530

    XAMARIN-35884 - TreeView ItemHold sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Behavior/Behavior.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        SfTreeView treeView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            treeView = bindable.FindByName<SfTreeView>("treeView");
+            treeView.ItemHolding += TreeView_ItemHolding;
+        }
+
+        private void TreeView_ItemHolding(object sender, ItemHoldingEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("Item Hold", "TreeView item is holding", "Close");
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            treeView.ItemHolding += TreeView_ItemHolding;
+        }
+    }
+}
+    
+
+
